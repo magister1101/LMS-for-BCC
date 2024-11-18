0881295d95cfea2ba4b36f07db1d8bb795d1787f
MarkVanMortem <34596876+magister1101@users.noreply.github.com>
2024-11-18 22:12:12 +0800

    rank system added to database

--- api/models/user.js
+++ api/models/user.js
@@ -3,50 +3,51 @@ const { type } = require('os');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    username: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    middleName: {type: String},
-    gender: {type: String, required: true},
-    contactNumber: {type: String, required: true},
-    birthDate: {type: Date, required: true},
-    school: {type: String, required: true},
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    middleName: { type: String },
+    gender: { type: String, required: true },
+    contactNumber: { type: String, required: true },
+    birthDate: { type: Date, required: true },
+    school: { type: String, required: true },
 
     //address
-    country: {type: String, required: true},
-    zipCode: {type: String, required: true},
-    province: {type: String, required: true},
-    municipality: {type: String, required: true},
-    barangay: {type: String, required: true},
-    street: {type: String, required: true},
-    blockAndLot: {type: String, required: true},
+    country: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    province: { type: String, required: true },
+    municipality: { type: String, required: true },
+    barangay: { type: String, required: true },
+    street: { type: String, required: true },
+    blockAndLot: { type: String, required: true },
 
     //guardian info
-    guardianFirstName: {type: String, required: true},
-    guardianLastName: {type: String, required: true},
-    guardianMiddleName: {type: String},
-    guardianContactNumber: {type: String, required: true},
-    
+    guardianFirstName: { type: String, required: true },
+    guardianLastName: { type: String, required: true },
+    guardianMiddleName: { type: String },
+    guardianContactNumber: { type: String, required: true },
+
     //guardian address
-    guardianCountry: {type: String, required: true},
-    guardianZipCode: {type: String, required: true},
-    guardianProvince: {type: String, required: true},
-    guardianMunicipality: {type: String, required: true},
-    guardianBarangay: {type: String, required: true},
-    guardianStreet: {type: String, required: true},
-    guardianBlockAndLot: {type: String, required: true},
+    guardianCountry: { type: String, required: true },
+    guardianZipCode: { type: String, required: true },
+    guardianProvince: { type: String, required: true },
+    guardianMunicipality: { type: String, required: true },
+    guardianBarangay: { type: String, required: true },
+    guardianStreet: { type: String, required: true },
+    guardianBlockAndLot: { type: String, required: true },
 
     //image
     qrCode: { type: String },
-    userImage: {type: String,required: true},
+    userImage: { type: String, required: true },
 
     //title
-    title: {type: String, required: true, default: "Member"},
+    title: { type: String, required: true, default: "Member" },
+    rank: { type: number, required: true, default: 0 },
 
     //archive checker
-    isArchived: {type: Boolean, default: false},
+    isArchived: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
